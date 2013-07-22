6aa2a0acc41123f46f60aeab37c73d3d6e89256a
Dario Garcia <dario@Darios-MacBook-Air.local>
2013-07-22 16:55:54 +1000

    Added bias removal functions

--- AMF.py
+++ AMF.py
@@ -56,8 +56,8 @@ def SGD(A, k, reg, NUM_ITER = 100, TOL = 0.001, lr = 1e-2, t0 = 100, BACKOFF_RAT
     # A.data = A.data - A.data.mean()
 
     # Random initialization
-    U = np.random.randn(Nu, k)
-    V = np.random.randn(k, Ni)
+    U = np.random.randn(Nu, k)/reg
+    V = np.random.randn(k, Ni)/reg
     
     support = np.vstack(A.nonzero())    
     err_old = np.inf
@@ -80,8 +80,8 @@ def SGD(A, k, reg, NUM_ITER = 100, TOL = 0.001, lr = 1e-2, t0 = 100, BACKOFF_RAT
         if (abs((err-err_old)/err_old)<TOL):
             print 'Convergence reached'         
             break
-        elif (err>err_old):
-            print 'Error increasing, backing off'
+        elif (np.isnan(err) or (err>err_old)):
+            print 'Backing off'
             U = np.array(U_old)
             V = np.array(V_old)
             lr = lr/anneal_rate
@@ -96,3 +96,120 @@ def SGD(A, k, reg, NUM_ITER = 100, TOL = 0.001, lr = 1e-2, t0 = 100, BACKOFF_RAT
     
 def sp_norm(A,approx):
     return norm(A.data[A.data!=0]-approx[A.nonzero()])/norm(A.data)
+
+def bias_correction(A, reg = 1, THRESH = 1e-2, MAX_ITER = 5):
+    '''
+    Perform bias correction by taking into account per-user and per-item biases
+
+    Minimizes \sum_{(u,i) \in S} (r_{ui}-(\mu+b_u+b_i))^2 + \lambda(\sum_u b_u^2+ \sum_i b_i^2
+    '''
+    
+    N_u, N_i = A.shape
+    
+    A = A.copy()
+    A.data = A.data-A.data.mean()
+    
+    ## Fixed-point iteration for finding user and item biases
+    # Initialize
+    b_u = np.zeros(N_u)
+    b_i = np.zeros(N_i)
+    t = 0
+
+    while 1:
+        t += 1
+        old_bu = np.array(b_u)
+        old_bi = np.array(b_i)
+
+        ## Update user biases        
+        for n in range(N_u):
+            aux = A.nonzero()[1][A.nonzero()[0]==n]
+            if (len(aux)==0):
+                continue
+            r = A[n,aux].A
+            bias = old_bi[aux]
+            b_u[n] = np.sum(r - bias)/(reg+len(aux))
+        
+        print np.max(b_u)
+        print np.min(b_u)
+
+        # Update item biases
+
+        for n in range(N_i):
+            aux = A.nonzero()[0][A.nonzero()[1]==n]
+            if (len(aux)==0):
+                continue
+            r = A[aux,n].A
+            bias = old_bu[aux]
+            b_i[n] = np.sum(r-bias)/(reg+len(aux))
+
+        print np.max(b_i)
+        print np.max(b_i)
+
+        if ((norm(b_u-old_bu)<THRESH) and (norm(b_i-old_bi)<THRESH)):
+            print 'Convergence reached'
+            return (b_u, b_i)
+
+        if (t >= MAX_ITER):
+            print 'Maximum number of iterations reached'
+            return (b_u, b_i)
+
+def bias_correction_sgd(A, reg = 1, THRESH = 1e-3, MAX_ITER = 50, lr = 0.01):
+    '''
+    Perform bias correction by taking into account per-user and per-item biases
+
+    Minimizes \sum_{(u,i) \in S} (r_{ui}-(\mu+b_u+b_i))^2 + \lambda(\sum_u b_u^2+ \sum_i b_i^2
+    '''
+    
+    N_u, N_i = A.shape
+    
+    A = A.copy()
+    A.data = A.data-A.data.mean()
+    
+    ## Stochastic Gradient Descent
+    # Initialize
+    b_u = np.zeros(N_u)
+    b_i = np.zeros(N_i)
+    t = 0
+    err_est = np.inf
+
+    support = np.vstack(A.nonzero())
+    N = support.shape[1]
+    err_vec = np.zeros(N)
+
+    while 1:
+        t += 1
+        old_bu = np.array(b_u)
+        old_bi = np.array(b_i)
+        old_err = err_est
+        
+        ## Update user biases        
+        err_est = 0
+        for n in range(support.shape[1]):
+            u,i = support[:,n]
+            pred = b_u[u] + b_i[i]
+            err = A.data[n]-pred
+            err_vec[n] = err
+            err_est += err**2
+            b_u[u] += lr*(err-reg*b_u[u])
+            b_i[i] += lr*(err-reg*b_i[i])
+
+        err_est = err_est/N
+        print 'Iteration %i\tEstimated error: %f' % (t,err_est)
+
+        if (err_est > old_err):
+            print 'Backing off ...'
+            b_u = np.array(old_bu)
+            b_i = np.array(old_bi)
+            lr = lr*0.75
+            continue
+
+        if (np.abs(err_est-old_err)<THRESH):
+            print 'Convergence reached'
+            A.data = err_vec
+            return (b_u, b_i, A)
+
+        if (t >= MAX_ITER):
+            print 'Maximum number of iterations reached'
+            A.data = err_vec
+            return (b_u, b_i, A)
+
--- data_load.py
+++ data_load.py
@@ -8,19 +8,27 @@ import pandas as pd
 import numpy as np
 from scipy.sparse import csr_matrix
 from os.path import join
-DATA_FOLDER= '/home/dario/Projects/RecommenderTest/ml-100k'
+from os import getcwd
+DATA_FOLDER= join(getcwd(),'ml-100k')
 DATA_FILE = 'u.data'
 
-def load_ml100k(max_ratings = None, normalize = 'user'):
+def load_ml100k(max_ratings = None, normalize = 'user', get_matrix = True, min_ratings = 3):
     # Read tab separated data into a dataframe
     df = pd.read_csv(join(DATA_FOLDER, DATA_FILE), sep = '\t', header = False)
     df.columns = ['user','item','rating','ts']
     if (max_ratings!=None):
         df = df.ix[:max_ratings]
-    if (normalize != None):
+
+    if (normalize == 'avg'):
+        df['rating'] = df['rating'] - df['rating'].mean()
+    elif (normalize != None):
         gr = df.groupby(normalize)
         df['rating'] = gr.transform(lambda(x): x - x.mean())['rating']
 
-    # Now transform it into a sparse matrix
-    A = csr_matrix((df.rating.values,df[['user','item']].values.T))
-    return A        
+        
+    if (get_matrix):
+        # Now transform it into a sparse matrix
+        A = csr_matrix((df.rating.values,df[['user','item']].values.T))
+        return A        
+
+    return df
